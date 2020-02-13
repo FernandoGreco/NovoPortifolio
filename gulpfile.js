14b878c882168f39a65ff5df0e471bce6a6b6e46
Fernando Greco <greco-fernando@hotmail.com>
2020-02-12 23:07:51 -0300

    alteração fundo

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const del = require('del');
+var concat = require('gulp-concat');
 
+gulp.task('concat', function() {
+	    return gulp.src(['src/js/maqEscrever.js','src/js/menu.js','src/js/vetorIds.js','src/js/editCss.js','src/js/luz.js'])
+      .pipe(concat('all.js'))
+      .pipe(gulp.dest('src/js'));
+  });
 
 gulp.task('styles', () => {
     return gulp.src('src/sass/*.scss')
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,12 +1,35 @@
-
+/*
 //chama função ao clicar em menu4
-document.getElementById("menus4").onclick = function() {myFunction()};
+document.getElementById("menus4").onclick || document.getElementById("menus").onclick = function() {fundo2()};
 
-function myFunction(){
+function fundo2(){
   console.log("menu 4 teste");
   document.getElementById("svgFundo").style.display = 'none';
   document.getElementById("svgFundoContato").style.display = 'inherit';
 }
+
+//chama função ao clicar em menu4
+document.getElementById("menus2").onclick || document.getElementById("menus3").onclick = function() {fundoSimples()};
+
+function fundoSimples(){
+  console.log("menu 5 teste");
+  document.getElementById("svgFundo").style.display = 'inherit';
+  document.getElementById("svgFundoContato").style.display = 'none';
+}
+*/
+
+document.getElementById("menus2").onclick = function() {fundo2()};
+document.getElementById("menus3").onclick = function() {fundo2()};
+
+//usar true e false para verificar se foi aplicado o fundo
+document.getElementById("menus").onclick = function() {fundoSimples()};
+document.getElementById("menus4").onclick = function() {fundoSimples()};
+
+function fundo2(){
+  console.log("menu sobre teste");
+  document.getElementById("svgFundo").style.display = 'inherit';
+}
+
 
 function menu(){
 
